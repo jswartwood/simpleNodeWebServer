61adffc1a2b123cbdb7447884b8bd67bc1d90cc4
Jacob Swartwood <jswartwood@gmail.com>
2011-01-27 00:48:53 -0500

    Deeper directory index.html checking.

--- simpleWebServer.js
+++ simpleWebServer.js
@@ -1,27 +1,34 @@
 var sys = require('sys'), 
 http = require('http')
 fs = require('fs');
+
+var PATH_TO_WWW = "www";
+
 http.createServer(function (req, res) {
 	setTimeout(function () {
-		var parsedReq = require("url").parse(req.url, true);
-		// node must be run from this dir for this to work
-		fs.readFile('www' + parsedReq.pathname, function( err, data ) {
+		var reqPath = require("url").parse(req.url, true).pathname;
+		// Fix this test to do an actual fs test if path is a dir
+		if (/\/$/.test(reqPath)) {
+			reqPath = reqPath + "index.html";
+		}
+		
+		fs.readFile(PATH_TO_WWW + reqPath, function( err, data ) {
 			if (err) {
 				res.writeHead(200, {'Content-Type': 'text/html'});
-				res.write('404');
-				res.close();
+				res.write('404: ' + reqPath);
+				res.end();
 			} else {
 				var ct = 'text/html';
-				if (/\.js(on)?$/.test(parsedReq.pathname)) {
+				if (/\.js(on)?$/.test(reqPath)) {
 					ct = 'text/javascript';
-				} else if (/\.css$/.test(parsedReq.pathname)) {
+				} else if (/\.css$/.test(reqPath)) {
 					ct = 'text/css';
 				} else {
 					// test for images
 				}
 				res.writeHead(200, {'Content-Type': ct});
 				res.write(data);
-				res.close();
+				res.end();
 			}
 		});
 	}, 200);
